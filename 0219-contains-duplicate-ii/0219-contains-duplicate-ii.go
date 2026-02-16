65f132abd306eaad9a5351187b01ebbb11c6f60d
Shuddhodhan Surwade <119784108+sidz111@users.noreply.github.com>
2026-02-16 23:33:48 +0530

    Time: 45 ms (28.07%), Space: 14.8 MB (23.74%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
@@ -0,0 +1,13 @@
+func containsNearbyDuplicate(nums []int, k int) bool {
+    m := make(map[int]int)
+
+    for i, num:= range nums{
+        if index, ok := m[num]; ok{
+            if i-index <= k{
+                return true
+            }
+        }
+        m[num]=i
+    }
+    return false
+}
