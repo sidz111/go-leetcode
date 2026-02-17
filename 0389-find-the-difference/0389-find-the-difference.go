fc76df3d57ef08885857624cb070585142012cbf
Shuddhodhan Surwade <119784108+sidz111@users.noreply.github.com>
2026-02-17 13:18:37 +0530

    Time: 0 ms (100%), Space: 4.1 MB (68.33%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.go
@@ -0,0 +1,13 @@
+func findTheDifference(s string, t string) byte {
+    var result byte
+
+    for i := 0; i < len(s); i++ {
+        result ^= s[i]
+    }
+
+    for i := 0; i < len(t); i++ {
+        result ^= t[i]
+    }
+
+    return result
+}
