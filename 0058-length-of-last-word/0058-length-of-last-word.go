c8f62ed4458a86fd9f9e48730ff2a6a6e3e27097
Shuddhodhan Surwade <119784108+sidz111@users.noreply.github.com>
2026-02-17 12:53:42 +0530

    Time: 0 ms (100%), Space: 4.2 MB (45.99%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.go
+++ 0058-length-of-last-word/0058-length-of-last-word.go
@@ -1,11 +1,27 @@
 func lengthOfLastWord(s string) int {
-    newStr := strings.TrimRight(s, " ")
-    count := 0
-    for i:=len(newStr)-1; i>=0; i--{
-        if newStr[i]==' '{
-            break
-        }
-        count ++
+    // newStr := strings.TrimRight(s, " ")
+    // count := 0
+    // for i:=len(newStr)-1; i>=0; i--{
+    //     if newStr[i]==' '{
+    //         break
+    //     }
+    //     count ++
+    // }
+    // return count
+
+
+    end := len(s) - 1
+
+    // skip trailing spaces
+    for end >= 0 && s[end] == ' ' {
+        end--
     }
-    return count
+
+    // find last space
+    start := end
+    for start >= 0 && s[start] != ' ' {
+        start--
+    }
+
+    return end - start
 }
